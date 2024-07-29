481acb2bf7d55d79c243bc1b914fe4995b78d911
Aifedayo <akeemifedayolag@gmail.com>
2024-07-29 21:18:54 +0100

    test deployment to test our instances

---
+++ src/test_deployment.py
@@ -0,0 +1,13 @@
+from .client_factory import RDSClient
+from .rds import RDS
+
+def deploy_resource():
+    rds_client = RDSClient().get_client()
+    rds = RDS(rds_client)
+
+    rds.create_postgresql_instance()
+    print('Creating RDS PostgreSQL instance...')
+
+
+if __name__ == '__main__':
+    deploy_resource()
