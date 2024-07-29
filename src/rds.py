9405a99db4646ac439a56d9c8ba3cb2600402f58
Aifedayo <akeemifedayolag@gmail.com>
2024-07-29 21:16:44 +0100

    create postgresql instance using create_db_instance

--- src/rds.py
+++ src/rds.py
@@ -4,13 +4,31 @@ from .ec2 import EC2
 RDS_DB_SUBNET_NAME = 'my-rds-subnet-group'
 
 
+def create_db_security_group_and_rules():
+    ec2_client = EC2Client().get_client()
+    ec2 = EC2(ec2_client)
+
+    # Create security group
+    security_group = ec2.create_security_group
+    # Get id of the sg
+    security_group_id = security_group['GroupId']
+
+    print('Created RDS Security group with id ' + security_group_id)
+
+    # Add public access rule to sg
+    ec2.add_inbound_rule_to_sg(security_group_id)
+
+    print("Added inbound public access rule to sg with id " + security_group_id)
+    return security_group_id
+
+
 class RDS:
     def __init__(self, client):
         self._client = client
         """ :type : pyboto3.rds """
 
     def create_postgresql_instance(self):
-        security_group_id = self.create_db_security_group_and_rules()
+        security_group_id = create_db_security_group_and_rules()
         print("Creating Amazon RDS PostgreSQL DB Instance")
         self._client.create_db_instance(
             DBName="MyPostgreSQLDB",
@@ -46,20 +64,3 @@ class RDS:
                 'subnet-018eb4d73923bb4aa'
             ]
         )
-
-    def create_db_security_group_and_rules(self):
-        ec2_client = EC2Client().get_client()
-        ec2 = EC2(ec2_client)
-
-        # Create security group
-        security_group = ec2.create_security_group
-        # Get id of the sg
-        security_group_id = security_group['GroupId']
-
-        print('Created RDS Security group with id ' + security_group_id)
-
-        # Add public access rule to sg
-        ec2.add_inbound_rule_to_sg(security_group_id)
-
-        print("Added inbound public access rule to sg with id " + security_group_id)
-        return security_group_id
