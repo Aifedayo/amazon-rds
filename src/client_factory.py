4db9a4a1e11baeffde6173a4957ae46574069eb6
Aifedayo <akeemifedayolag@gmail.com>
2024-07-29 18:41:18 +0100

    RDSClient inherits the ClientFactory which is used to get the rds as a client

--- src/client_factory.py
+++ src/client_factory.py
@@ -1,8 +1,14 @@
 import boto3
 
+
 class ClientFactory:
     def __init__(self, client):
         self._client = boto3.client(client, region_name="us-east-1")
 
     def get_client(self):
         return self._client
+
+
+class RDSClient(ClientFactory):
+    def __init__(self):
+        super().__init__('rds')
